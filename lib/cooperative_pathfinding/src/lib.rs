121fce6be04515f59d46995c5016999c88f04603
bruno <debaetsbruno@gmail.com>
2021-10-01 20:51:06 +0200

    Complete non-tested true heuristic function

--- lib/cooperative_pathfinding/src/lib.rs
+++ lib/cooperative_pathfinding/src/lib.rs
@@ -1,6 +1,7 @@
 use std::cmp::Reverse;
 use std::collections::HashMap;
 use priority_queue::priority_queue::PriorityQueue;
+use std::f32::consts::SQRT_2;
 
 pub struct WorldMap {
     pub data: Vec<u32>,
@@ -9,6 +10,7 @@ pub struct WorldMap {
 }
 
 impl WorldMap {
+
     pub fn new(data: Vec<u32>, width: u32, height: u32) -> WorldMap {
         WorldMap {
             data,
@@ -18,7 +20,7 @@ impl WorldMap {
     }
 
     pub fn get_cost(&self, node: Node) -> u32 {
-        self.map[(goal.y * map.width + goal.x) as usize]
+        self.data[(node.y * self.data.width + node.x) as usize]
     }
 
     pub fn get_neighbors(&self, position: Node) -> Vec<Node> {
@@ -66,6 +68,12 @@ impl WorldMap {
         neighbors
 
     }
+
+    // We use Manhattan distance to calculate
+    // right angle distance between start and goal
+    pub fn manhattan_distance(a: Node, b: Node) -> u32 {
+        (i32::abs(a.x - b.x) + i32::abs(a.y - b.y))
+    }
 }
 
 #[derive(Clone, Copy, Default)]
@@ -87,6 +95,12 @@ impl From<(u32, u32, u32)> for Node {
     }
 }
 
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> Self {
+        self.x == other.x || self.y == other.y
+    }
+}
+
 #[derive(Default)]
 pub struct Agent {
 
@@ -96,7 +110,7 @@ pub struct Agent {
     pub closed_set: HashMap<Node, usize>,
     // This priority queue will be ordered by the reverse of the highest cost
     // so, the priority of nodes exploration will depend of their accessibility cost
-    pub open_set: PriorityQueue<(usize, usize), Reverse<usize>>,
+    pub open_set: PriorityQueue<Node, Reverse<usize>>,
 
     name: String,
     start: Node,
@@ -142,17 +156,24 @@ impl Agent {
         let mut start = goal;
         let goal =  agent.start;
 
+        let is_goal_found = false;
+
         if map.get_cost(goal) == u32::MAX {
             false
         }
 
+        agent.g_score.insert(start, 0);
+        agent.f_score.insert(start, 0);
         self.open_set.push(start, Reverse(0));
 
         while let Some((current, Reverse(_current_cost))) = self.open_set.pop() {
 
-            for neighbor in map.get_neighbors(current) {
+            if current == goal {
+                true
+            }
+
+            for next in map.get_neighbors(current) {
                 match self.came_from.get(&neighbor) {
-                    Some(node) => {}
                     None => {
                         self.came_from.insert(current, neighbor);
                         self.g_score.insert(neighbor, u32::MAX);
@@ -160,21 +181,28 @@ impl Agent {
                     }
                 }
 
-                let new_cost = map.get_cost(current) + map.get_cost(neighbor)
+                let new_cost = {
+                    // We take the current node cost incremented
+                    // from the cost of next node
+                    // If diagonal, add an extra cost for traversing
+                    if current.x != neighbor.x && current.x != next.y {
+                        current.g_score + (map.get_cost(next) as f32 * SQRT_2) as u32
+                    }else{
+                        current.g_score + map.get_cost(next)
+                    }
+                };
 
-                if new_cost < neighbor.g_score {
-                    neighbor.g_score = ;
+                if agent.closed_set.get(neighbor).is_none() && new_cost < neighbor.g_score {
+
+                    next.g_score = new_cost;
+                    next.f_score = next.g_score + WorldMap::manhattan_distance(current, next);
+
+                    //Update priority queue with this new cost
+                    self.open_set.push_decrease(next, next.f_score);
+
                 }
             }
-
         }
-
-        true
+        false
     }
-}
-
-// We use Manhattan distance to calculate
-// right angle distance between start and goal
-fn manhattan_distance(a: (i32, i32), b: (i32, i32)) -> usize {
-    (i32::abs(a.0 - b.0) + i32::abs(a.1 - b.1)) as usize
 }
